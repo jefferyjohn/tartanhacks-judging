305870ea86a27f8222a56f9312983177627463e9
Gram Liu <gram.liu226@gmail.com>
2022-01-22 13:50:35 -0500

    Added judging url

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const isProduction = true
+const backends = {
+  local: "https://backend.tartanhacks.com",
+  development: "https://stg-backend.tartanhacks.com",
+  staging: "https://stg-backend.tartanhacks.com",
+  production: "https://backend.tartanhacks.com"
+}
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: false,
   env: {
-    BACKEND_URL: isProduction
-      ? "https://backend.tartanhacks.com"
-      : "http://localhost:4000"
+    BACKEND_URL: backends[process.env.NODE_ENV ?? "production"],
+    JUDGING_URL: "https://judging-backend.herokuapp.com/"
   }
 }
